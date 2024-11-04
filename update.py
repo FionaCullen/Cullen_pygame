9f9dfec7ca7835a82f6975d855dc7f419b6bed93
FionaCullen <m271356@usna.edu>
2024-11-04 08:44:44 -0500

    Added Bottom Rocks

--- update.py
+++ update.py
@@ -31,10 +31,22 @@ background.blit(sky, (0,0))
 grass_y = HEIGHT-grass.get_height()
 background.blit(grass, (0,grass_y))
 
-# Add rocks
-rock_width = rock_grass.get_width()
+# Add bottom rocks 
 rock_height = rock_grass.get_height()
+rock_y = HEIGHT - rock_height
 
+max_height = 250 
+rocks = []
+for _ in range(5):  # Create 10 rocks
+    rock_width = random.randint(50, 150)  # Random width between 50 and 150
+    rock_height = random.randint(150, max_height)  # Random height between 10 and max_height
+    rock_grass_scaled = pygame.transform.scale(rock_grass,(rock_width, rock_height))
+    rock_x = random.randint(0, WIDTH - rock_width)  # Random x position
+    rock_y = HEIGHT - rock_height  # Position at the bottom
+    rocks.append((rock_grass_scaled, rock_x, rock_y))
+
+
+     
 def scroll_background(speed): 
     global background_x
     background_x -= speed 
@@ -55,6 +67,9 @@ while running:
     screen.blit(background, (background_x, 0))
     screen.blit(background, (background_x + WIDTH, 0))
     
+    # Add bottom rocks 
+    for rock_grass_scaled, rock_x, rock_y in rocks: 
+        screen.blit(rock_grass_scaled, (rock_x, rock_y))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
